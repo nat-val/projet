79e4c3a365aecada6f455da2f5793311c6ab9312
nathalie-val <nathalie-val@wanadoo.fr>
2014-11-05 12:02:40 +0100

    vancement controller et views

--- src/Proj/ArticleBundle/Controller/ArticleController.php
+++ src/Proj/ArticleBundle/Controller/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace Proj\ArticleBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ArticleController extends Controller
@@ -11,13 +13,27 @@ class ArticleController extends Controller
         return $this->render('ProjArticleBundle:Article:index.html.twig');
     }
 	
-	 public function viewAction($id)
+	public function viewAction($id)
     {
         return $this->render('ProjArticleBundle:Article:view.html.twig', array('article_id' => $id));
     }
 	
-	 public function addAction()
+	public function addAction(Request $request)
     {
-        return $this->render('ProjArticleBundle:Article:add.html.twig');
+		$session = $request->getSession();
+		$session->getFlashBag()->add('info', 'Article bien enregistré');
+
+		return $this->redirect($this->generateUrl('proj_article_view', array('id' => 5)));
+        //return $this->render('ProjArticleBundle:Article:add.html.twig');
+    }
+	
+	public function editAction($id)
+    {
+        return $this->render('ProjArticleBundle:Article:edit.html.twig', array('article_id' => $id));
+    }
+	
+	public function deleteAction($id)
+    {
+        return $this->render('ProjArticleBundle:Article:delete.html.twig', array('article_id' => $id));
     }
 }
