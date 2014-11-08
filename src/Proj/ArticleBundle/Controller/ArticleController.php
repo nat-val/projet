821eb78307cb1685494e35b22ff7d3049026d8d7
nathalie-val <nathalie-val@wanadoo.fr>
2014-11-08 21:56:50 +0100

    Ajout templates, avant bootstrap

--- src/Proj/ArticleBundle/Controller/ArticleController.php
+++ src/Proj/ArticleBundle/Controller/ArticleController.php
@@ -2,14 +2,19 @@
 
 namespace Proj\ArticleBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ArticleController extends Controller
 {
-    public function indexAction()
+    public function indexAction($page)
     {
+		if ($page < 1) {
+		  // On déclenche une exception NotFoundHttpException, cela va afficher
+		  // une page d'erreur 404
+		  throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+		}
         return $this->render('ProjArticleBundle:Article:index.html.twig');
     }
 	
@@ -36,4 +41,14 @@ class ArticleController extends Controller
     {
         return $this->render('ProjArticleBundle:Article:delete.html.twig', array('article_id' => $id));
     }
+	
+	public function menuAction()
+	{
+		$articles_list = array(
+		    array('id' => 2, 'title' => 'La recette de la brioche'),
+		    array('id' => 5, 'title' => "L'arc en ciel"),
+		    array('id' => 9, 'title' => 'Astronomie pour débutant')
+		);
+		return $this->render('ProjArticleBundle:Article:menu.html.twig', array('articles_list' => $articles_list));
+	}
 }
